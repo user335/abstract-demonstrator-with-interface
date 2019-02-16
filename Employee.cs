ad49aff838544f29de03e01cd3cb38a71b064530
Andy Weekes <userthreethreefive@gmail.com>
2019-02-15 17:55:51 -0700

    correcting another error

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("\r\nGoodbye");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public abstract void SayName();
+
+        static void Main()
+        {
+            var employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+            Console.Read();
+
+            IQuittable quitter = new Employee();
+            quitter.Quit();
+        }
+    }
+}
